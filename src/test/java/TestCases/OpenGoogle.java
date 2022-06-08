5895fd7b25462ac3ee84859b064eb1dffd4590ec
91arijitghosh <91arijit.ghosh@gmail.com>
2022-06-08 21:03:12 +0530

    First Commit

---
+++ src/test/java/TestCases/OpenGoogle.java
@@ -0,0 +1,22 @@
+package TestCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class OpenGoogle
+{
+    @Test
+    public void open_Google()
+    {
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.google.com");
+
+    }
+}
