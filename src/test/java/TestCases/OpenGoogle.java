54d034cb95063255610421419613131d594a1d61
Arijit Ghosh <53399683+91arijitghosh@users.noreply.github.com>
2022-11-14 09:40:40 +0530

    Update OpenGoogle.java

--- src/test/java/TestCases/OpenGoogle.java
+++ src/test/java/TestCases/OpenGoogle.java
@@ -11,17 +11,17 @@ public class OpenGoogle
     @Test
     public void open_Google()
     {
-        //*
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.google.com");
-        System.out.println(driver.getTitle());
+        
+        //System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\chromedriver.exe");
+        //WebDriver driver = new ChromeDriver();
+        //driver.manage().window().maximize();
+        //driver.manage().deleteAllCookies();
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.get("https://www.google.com");
+        //System.out.println(driver.getTitle());
 
-        driver.quit();
-*//
+        //driver.quit();
+
        System.out.println("This is a demo Maven Project");
     }
 }
