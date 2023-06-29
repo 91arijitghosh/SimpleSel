d66c994dcbdffc4bc464f0c981c8ce934cd8b8b3
Arijit Ghosh <53399683+91arijitghosh@users.noreply.github.com>
2023-06-29 14:16:44 +0530

    Update OpenGoogle.java

--- src/test/java/TestCases/OpenGoogle.java
+++ src/test/java/TestCases/OpenGoogle.java
@@ -1,6 +1,9 @@
 package TestCases;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -11,9 +14,21 @@ public class OpenGoogle
     @Test
     public void open_Google()
     {
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        cap.setBrowserName("chrome");
+        cap.setPlatform(Platform.LINUX);
+        
         System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/main/resources/chromedriver");
         //System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/main/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        //options.addArguments("--disable-dev-shm-usage");
+        //options.addArguments("--headless");
+        options.setBinary("/usr/bin/google-chrome");
+        options.merge(cap);
+        
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
